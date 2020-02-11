c5e11abb80626bb0f0493887ee299de027b2ab76
delevan <michael.delevan@scranton.edu>
2020-02-11 09:14:11 -0500

    DS 201 Report

--- data-scraper/data-scraper.py
+++ data-scraper/data-scraper.py
@@ -1,6 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
+teamAbbr = ["CHN", "PHI", "PIT", "CIN", "SLN", "BOS", "CHA",
+                "CLE", "DET", "NYA", "BAL", "LAN", "SFN", "MIN",
+                "HOU", "NYN", "ATL", "OAK", "KCA", "SDN", "TEX",
+                "TOR", "SEA", "FLO", "COL", "ANA", "TBA", "ARI",
+                "MIL", "WAS"]
+url = "http://www.baseball-reference.com/teams/tgl.cgi?team=" + teamAbbr + "&t=b&year=" + str(year)
+
 URL = 'https://www.baseball-reference.com/teams/tgl.cgi?team=CHC&t=b&year=2019#rowsum_desc'
 page = requests.get(URL)
 
@@ -8,7 +15,7 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 results = soup.find(id='team_batting_gamelogs')
 
-#games = results.find_all('section', class_='left')
+stats = soup.find_all('td', attrs = {'data-stat': 'RBI'})
 
-print(results.prettify())
+print(stats)
 
